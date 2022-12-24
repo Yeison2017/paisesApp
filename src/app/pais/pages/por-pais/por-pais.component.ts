151c8147f0577b486ca9d6783da527b1a02d5cc0
yeison2017 <yha.big@gmail.com>
2022-12-24 13:30:08 -0500

    Llenar la tabla de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,17 +10,23 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) {}
 
   buscar() {
-    this.hayError= false;
+    this.hayError = false;
 
     console.log(this.termino);
-    this.paisService.buscarPais(this.termino).subscribe((resp) => {
-      console.log(resp);
-    }, err => {
-      this.hayError = true;
-    });
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
 }
