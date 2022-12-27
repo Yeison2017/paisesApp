0bbd9101fe7634d561718f2b85e4b2e2c42440fa
yeison2017 <yha.big@gmail.com>
2022-12-27 17:14:55 -0500

    Ver pais de forma independiente

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {});
+  }
 }
